d6ac4d189da3e195b9863a875a064438e20af050
Jiayu Zhou <dearjiayu@gmail.com>
2018-03-25 22:49:06 -0400

    learning' by mar 25

--- koans/about_hashes.rb
+++ koans/about_hashes.rb
@@ -97,12 +97,14 @@ class AboutHashes < Neo::Koan
 
     hash[:one] << "uno"
     hash[:two] << "dos"
+    #jiayu note: these two worked on the same [] object created 
+    #    in line 96. 
 
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
+    assert_equal ["uno", "dos"], hash[:one]
+    assert_equal ["uno", "dos"], hash[:two]
+    assert_equal ["uno", "dos"], hash[:three]
 
-    assert_equal __, hash[:one].object_id == hash[:two].object_id
+    assert_equal true, hash[:one].object_id == hash[:two].object_id
   end
 
   def test_default_value_with_block
@@ -110,9 +112,10 @@ class AboutHashes < Neo::Koan
 
     hash[:one] << "uno"
     hash[:two] << "dos"
+    # jiayu note: hash[key] = [] gives a new array to each hash 
 
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
+    assert_equal ["uno"], hash[:one]
+    assert_equal ["dos"], hash[:two]
+    assert_equal [], hash[:three]
   end
 end
